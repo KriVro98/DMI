31a326812e94a0c9dfd9bbb1b81b8fb611dc2be3
rastamaha1998@gmail.com <kristers2345@inbox.lv>
2017-11-09 09:52:55 +0200

    20171109_09_50

---
+++ PYTHON/dialogs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+a = input("Lietotaj, ludzu, ievadi kaut ko: ")
+print "Tu ievadiji lielumu ar %s dat tipu"%(type(a))
+#print a * a
+print a + a
+
+a = raw_input("Lietotaj, ludzu, ievadi kaut ko: ")
+print "Tu ievadiji lielumu ar %s dat tipu"%(type(a))
+#print a * a
+print a + a
+'''
+
+
+a = raw_input("Lietotaj, ludzu, ievadi savu vardu: ")
+b = raw_input("Lietotaj, ludzu, ievadi savu uzvardu: ")
+print "Tatad, Tevi sauc -%s"%(a+b)
+print "Tatad, Tevi sauc -%s"%(a + chr(32) + b)
